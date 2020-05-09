e9e8cad4e67c22d6039df701ec8dec139f9cf8fc
cp2296328845 <2296328845@qq.com>
2020-05-09 22:05:33 +0800

    add

--- create_data.py
+++ create_data.py
@@ -5,6 +5,7 @@
 # @Site    : 
 # @File    : create_data.py
 # @Software: PyCharm
+from sklearn.cross_validation import train_test_split
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import pandas as pd
@@ -18,13 +19,53 @@ def read_image(file_path,width,height):
 
 def create_data(sampleNumaber,version):
     imageErrorList = []
-    # data = np.ndarray((sample_num, 80, 80, 3), dtype=np.float32)
-    for i in range(1,13):
-        file_name = "./label/"+str(i)+"_namelbel.csv"
-        print(file_name)
-        imageNames = pd.read_csv("label/12_namelbel.csv")
+    data = np.ndarray((sampleNumaber*12, 112, 112, 3), dtype=np.float32)
+    # print(data.shape)
+    for i in range(12):
+        file_name = "./label/"+str(i+1)+"_namelbel.csv"
+        # print(file_name)
+        name = pd.read_csv(file_name,header=0,sep=',')
+        imageNames = np.array(name['image_name'])
+        number = 0
         for image in imageNames:
-            print(image)
+            image_path = "./faces_change/"+image+"_1.jpg"
+            print(str(number) + ":" + image)
+            try:
+                img=read_image(image_path,112,112)
+                key = sampleNumaber * i + number
+                data[key] = img
+                print(key)
+                number += 1
+            except:
+                imageErrorList.append(image_path)
+                print(image_path+"error!!!!!")
+            if number == sampleNumaber:
+                break
+
+    dataframe = pd.DataFrame({'image_name': imageErrorList})
+    csv_name = "error.csv"
+    dataframe.to_csv(csv_name, mode='a', sep=',')
+    np.save("image_data_"+str(version)+".npy",data)
+
+def create_label(sampleNumaber,version):
+    label = np.ndarray((sampleNumaber*12, 12), dtype=np.float32)
+    for i in range(12):
+        for num in range(sampleNumaber):
+            label[sampleNumaber * i + num][i] = 1
+    np.save("image_label_" + str(version) + ".npy", label)
+
 
 if __name__ == '__main__':
-    create_data(10,10)
+    # create_data(2000,0)
+    # create_label(2000,0)
+    num =2000
+    version =0
+    images = np.load("./image_data_" + str(version) + ".npy")
+    # images = preprocessing.scale(images)
+    # images = images.reshape(-1, 128, 14)
+    label = np.load("./image_label_" + str(version) + ".npy")
+    X_train, X_test, Y_train, Y_test = train_test_split(images, label, test_size=0.1)
+    np.save("./image_X_train"+str(num)+"_"+str(version)+".npy", X_train)
+    np.save("./image_X_test"+str(num)+"_"+str(version)+".npy", X_test)
+    np.save("./image_Y_train"+str(num)+"_"+str(version)+".npy", Y_train)
+    np.save("./image_Y_test"+str(num)+"_"+str(version)+".npy", Y_test)
